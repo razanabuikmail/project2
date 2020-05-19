8eb04c2806b9f43704296cd37b0fa47d58aa7dce
Razan Abu ikmail <razanabuikmail8@gmail.com>
2020-05-19 21:02:15 +0300

    th

--- 3.js
+++ 3.js
@@ -1,6 +1,6 @@
 function currencyConverter(valNum){
 if(converter.num.value <= 0)
-window.alert("Enter Value Greaterthan 0");
+window.alert("NO currency to convert");
 else{
 var us = document.getElementById("dollar").value = (valNum*0.283859);
 var jo =document.getElementById("dinar").value = (valNum*0.201119);
@@ -10,5 +10,14 @@ var Er =document.getElementById("Eoro").value = (valNum*0.259238);
 
 // function addToRecorder(addtd)
 // var t = document.getElementById('table');
-//  document.getElementById("table").insertRow(-1).innerHTML 
-//= '<td>("dollar").value</td> <td>jo</td> <td>Er</td>'
+//  document.getElementById("table").insertRow(-1).innerHTML
+// //= '<td>("dollar").value</td> <td>jo</td> <td>Er</td>'
+// var td = document.createElement("td").append(table)
+
+ function displayResult()
+        {
+                      document.getElementById("table").insertRow(-1).td.innerHTML = '<td>("dollar").value</td> <td>jo</td> <td>Er</td>'
+          
+
+          
+        }
